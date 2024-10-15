780ed363f0abc9af9b6847545f1d2f7d30729e87
DevDeNiro <oldoni.jeremy@outlook.fr>
2024-10-15 16:01:15 +0200

    update Home.tsx

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,10 +2,23 @@ import { User } from "firebase/auth";
 import { FC } from "react";
 import { HomeWrapper } from "@components/pages/Home/Home.styled.tsx";
 import { FormattedMessage, useIntl } from "react-intl";
+import SearchBar from "@molecules/Searchbar/Searchbar.tsx";
+import Feed from "@organisms/Feed/Feed.tsx";
+import data from "@utils/data/twitter-data.ts";
 
 interface HomeProps {
     user?: User | null;
 }
+
+export type Post = {
+    id: string;
+    username: string;
+    handle: string;
+    content: string;
+    likes: number;
+};
+
+const fetchedPosts = data;
 
 const Home: FC<HomeProps> = ({ user }) => {
     const intl = useIntl();
@@ -13,6 +26,10 @@ const Home: FC<HomeProps> = ({ user }) => {
     const userStatusMessage = intl.formatMessage({
         id: user ? "app.header.status.loggedIn" : "app.header.status.loggedOut",
     });
+
+    const handleSearch = (query: string) => {
+        console.log(`Search query: ${query}`);
+    };
 
     return (
         <HomeWrapper>
@@ -30,6 +47,9 @@ const Home: FC<HomeProps> = ({ user }) => {
                 />
             </h2>
             {user && <h3>{user.email}</h3>}
+
+            <SearchBar onSearch={handleSearch} />
+            <Feed posts={fetchedPosts} />
         </HomeWrapper>
     );
 };
